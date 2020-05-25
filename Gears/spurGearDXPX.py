478dbe47347509f4a03ea58ba43366a150666cfa
ccgarant <ccgarant@gmail.com>
2020-05-25 14:21:01 -0400

    updates to notebooks and supporting .py files

--- Gears/spurGearDXPX.py
+++ Gears/spurGearDXPX.py
@@ -1,30 +1,4 @@
 # spur gear design
-'''
-Inputs: 
-
-d = 4, [in] pitch circle diameter
-P = 4, [in] diametral pitch
-N = 16, # of gear teeth
-PA = 27, [deg] pressure angle
-
-Outputs:
-
-r_pitch= d/2 #pitch radius
-r_base = round((r_pitch)*cos(deg2rad(pa)),3)
-addendum = 1.00/P  #addendum
-dedendum = 1.25/P  #dedendum
-clearance = .125
-r_addendum = r_pitch + addendum
-r_dedendum = r_pitch - dedendum
-r_clearance = r_pitch - dedendum + clearance
-tooth_thickness = p/2 #circular pitch divided by two
-s = tooth_thickness  #arc
-r = r_pitch
-theta_tooth_thickness_rad = s/r_pitch
-theta_tooth_thickness_deg = round(np.rad2deg(theta_tooth_thickness_rad),3)
-theta_tooth_thickness_lineofsymmetry_deg = theta_tooth_thickness_deg/2     #line of symmetry about 
-
-'''
 
 #imports
 from numpy import pi, sin, cos, tan, deg2rad, arange
@@ -32,18 +6,53 @@ import pandas as pd
 import numpy as np
 
 
-#####d
-# dims
+#####
+# design
 #####
 
 #all dimensions in inches
 
 def spurGearDesign(d,P,PA):
+    
+    '''
+    # spur gear design helper function
+
+    Inputs: 
+
+    d = 4, [in] pitch circle diameter
+    P = 4, [in] diametral pitch
+    N = 16, # of gear teeth
+    PA = 27, [deg] pressure angle
+
+    Outputs:
+
+    r_pitch= d/2 #pitch radius
+    r_base = round((r_pitch)*cos(deg2rad(pa)),3)
+    addendum = 1.00/P  #addendum
+    dedendum = 1.25/P  #dedendum
+    clearance = .125
+    r_addendum = r_pitch + addendum
+    r_dedendum = r_pitch - dedendum
+    r_clearance = r_pitch - dedendum + clearance
+    tooth_thickness = p/2 #circular pitch divided by two
+    s = tooth_thickness  #arc
+    r = r_pitch
+    theta_tooth_thickness_rad = s/r_pitch
+    theta_tooth_thickness_deg = round(np.rad2deg(theta_tooth_thickness_rad),3)
+    theta_tooth_thickness_lineofsymmetry_deg = theta_tooth_thickness_deg/2     #line of symmetry about 
+
+    '''
     #inputs
     d = d  #pitch diameter
     P = P  #diametral pitch
     PA = PA #[deg] common is 20, but going for exagerated
     
+    
+    #####
+    # design
+    #####
+
+
     #outputs
     N = round(P*d,3) #number of teeth
     p = round(pi*d/N,3) #circular pitch
@@ -63,50 +72,54 @@ def spurGearDesign(d,P,PA):
     theta_tooth_thickness_rad = s/r_pitch
     theta_tooth_thickness_deg = round(np.rad2deg(theta_tooth_thickness_rad),3)
     theta_tooth_thickness_lineofsymmetry_deg = theta_tooth_thickness_deg/2     #line of symmetry about which to mirrow the invlute curve
+    
+    #####
+    # involute
+    #####
 
-print('gear design input parameters:')
-print('')
-print(f'{d}, pitch diameter')
-print(f'{P}, diametral pitch')
-print(f'{N}, number of teeth')
-print(f'{p}, circular pitch')
-print(f'{pa}, pressure angle')
-print('')
-print('gear design output design parameters:')
-print('')
-print('design info:')
-print(f'{addendum}, addendum, radial distance from pitch radius')
-print(f'{dedendum}, dedendum, radial distance from pitch radius')
-print(f'{clearance}, clearance')
-print('')
-print('radii info:')
-print(f'{r_pitch}, pitch radius')
-print(f'{r_base}, base radius')
-print(f'{r_addendum}, addendum radius')
-print(f'{r_dedendum}, dedendum radius')
-print(f'{r_clearance}, clearance radius')
-print('')
-print('tooth thickness info:')
-print(f'{tooth_thickness}, tooth thickness')
-print(f'{theta_tooth_thickness_deg} tooth thickness angle [deg]')
-print(f'{theta_tooth_thickness_lineofsymmetry_deg} tooth thickness line of symmetry [deg]')
+    theta_deg = arange(0,70,5)  #0 to 70 degress in 5 degree intervals
+    theta_rad = np.deg2rad(theta_deg)
+    s = theta_rad*r_base
 
-#####
-# involute
-#####
+    theta_deg = list(theta_deg)
+    theta_rad = list(theta_rad)
+    s = list(s)
 
-theta_deg = arange(0,70,5)  #0 to 70 degress in 5 degree intervals
-theta_rad = np.deg2rad(theta_deg)
-s = theta_rad*r_base
+    inv = pd.DataFrame(index=theta_deg, columns=['theta_rad','s arc length'])
+    inv.index.name = 'deg'
 
-theta_deg = list(theta_deg)
-theta_rad = list(theta_rad)
-s = list(s)
+    inv['theta_rad'] = theta_rad
+    inv['s arc length'] = s
+    
+    print('gear design input parameters:')
+    print('')
+    print(f'{d}, pitch diameter')
+    print(f'{P}, diametral pitch')
+    print(f'{N}, number of teeth')
+    print(f'{p}, circular pitch')
+    print(f'{PA}, pressure angle')
+    print('')
+    print('gear design output design parameters:')
+    print('')
+    print('design info:')
+    print(f'{addendum}, addendum, radial distance from pitch radius')
+    print(f'{dedendum}, dedendum, radial distance from pitch radius')
+    print(f'{clearance}, clearance')
+    print('')
+    print('radii info:')
+    print(f'{r_pitch}, pitch radius')
+    print(f'{r_base}, base radius')
+    print(f'{r_addendum}, addendum radius')
+    print(f'{r_dedendum}, dedendum radius')
+    print(f'{r_clearance}, clearance radius')
+    print('')
+    print('tooth thickness info:')
+    print(f'{tooth_thickness}, tooth thickness')
+    print(f'{theta_tooth_thickness_deg} tooth thickness angle [deg]')
+    print(f'{theta_tooth_thickness_lineofsymmetry_deg} tooth thickness line of symmetry [deg]')
+    print('')
+    print('involute curve:')
+    print(inv)
 
-inv = pd.DataFrame(index=theta_deg, columns=['theta_rad','s arc length'])
-inv.index.name = 'deg'
 
-inv['theta_rad'] = theta_rad
-inv['s arc length'] = s
 
-print(inv)
